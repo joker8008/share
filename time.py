028601526088133d3bddb8314b39511c0cb58007
joker8008 <141602802+joker8008@users.noreply.github.com>
2023-08-07 12:01:37 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,32 +1,36 @@
 import time
-import multiprocessing
 import queue
+import multiprocessing
+
 
 def countdown_process(q):
-    time_left = 20
-
-    while time_left > 0:
+    countdown = 20
+    while countdown > 0:
         try:
             data = q.get(timeout=1)
+            print(f"Received data from readtime.py: {data}")
             num = int(data)
-            print("接收到队列信息:", num)
-            if num >= 0:
-                time_left = num
-                print("重新设置倒计时时间为:", time_left, "秒")
-            else:
-                print("无效的倒计时时间，忽略:", num)
-        except queue.Empty:  # 使用正确的异常类型
+            countdown += num
+            print(f"Remaining time: {countdown} seconds")
+        except queue.Empty:
             print("No data received from readtime.py in the last 5 seconds.")
+        except ValueError:
+            print(f"Invalid data received: {data}")
 
+        countdown -= 1
         time.sleep(1)
-        time_left -= 1
+
+    # Countdown is completed, now handle the remaining data in the queue
+    while not q.empty():
+        data = q.get()
+        print(f"Received data from readtime.py after countdown: {data}")
+        num = int(data)
+        countdown += num
+        print(f"Remaining time: {countdown} seconds")
+
+    print("Countdown completed.")
+
 
 if __name__ == "__main__":
     q = multiprocessing.Queue()
-    process = multiprocessing.Process(target=countdown_process, args=(q,))
-    process.start()
-
-    try:
-        process.join()
-    except KeyboardInterrupt:
-        process.terminate()
+    countdown_process(q)
